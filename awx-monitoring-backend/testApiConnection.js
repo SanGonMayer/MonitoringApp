8cc1aac4725661415e38c232368ede1dfcc52627
Gonzalez Mayer <segmayer@bancocredicoop.coop>
2024-09-13 13:29:40 -0300

    conexion funcionando

---
+++ awx-monitoring-backend/testApiConnection.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+// URL de la API de AWX
+const awxApiUrl = 'http://sawx0001lx.bancocredicoop.coop/api/v2/hosts/';  // Ajusta la URL si es necesario
+
+// Credenciales de autenticación
+const username = 'segmayer';
+const password = '';  // La contraseña decodificada
+
+// Función para probar la conexión
+async function testApiConnection() {
+  try {
+    const response = await axios.get(awxApiUrl, {
+      auth: {
+        username: username,
+        password: password
+      }
+    });
+    
+    console.log('Datos de la API de AWX:', response.data);
+  } catch (error) {
+    console.error('Error al conectar con la API de AWX:', error.message);
+  }
+}
+
+// Ejecuta la función de prueba
+testApiConnection();
