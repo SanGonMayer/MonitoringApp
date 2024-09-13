57eccb48396f43ba5245bfaaa3fe493542c92efd
Gonzalez Mayer <segmayer@bancocredicoop.coop>
2024-09-13 18:17:37 -0300

    prueba exponer front 3.2

--- awx-monitoring-backend/testApiConnection2.js
+++ awx-monitoring-backend/testApiConnection2.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');  // <--- Importa cors
 const axios= require('axios');
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
+
+app.use(cors());  // <--- Usa el middleware CORS
+
 const awxApiUrl = 'http://sawx0001lx.bancocredicoop.coop/api/v2/hosts/'; 
 
 app.get('/api/awx/hosts', async (req, res) => {
-    try{
+    try {
         const awxResponse = await axios.get(awxApiUrl, {
             auth: {
                 username: 'segmayer',
@@ -21,7 +25,7 @@ app.get('/api/awx/hosts', async (req, res) => {
         console.error('Error al conectar a la API de AWX: ', error.message);
         res.status(500).json({ error: 'Error al conectar a la API de AWX'});
     }
-})
+});
 
 app.listen(PORT, () => {
     console.log('Servidor escuchando');
