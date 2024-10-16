b582be8d1f6bcf4a4580fc62fca8a7776f1c4ed1
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-10-16 11:45:03 -0300

    ref cir

---
+++ awx-monitoring-backend/models/index.js
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize';
+import sequelize from '../config/database.js';
+
+import Filial from './filiales.js';
+import Inventory from './inventory.js';
+import Workstation from './workstations.js';
+import CCTV from './cctv.js';
+import Job from './jobs.js';
+import JobHostSummary from './jobHostSummary.js';
+
+
+Filial.hasMany(Workstation, { foreignKey: 'filial_id' });
+Workstation.belongsTo(Filial, { foreignKey: 'filial_id' });
+
+Filial.hasMany(CCTV, { foreignKey: 'filial_id' });
+CCTV.belongsTo(Filial, { foreignKey: 'filial_id' });
+
+Inventory.hasMany(Workstation, { foreignKey: 'inventory_id' });
+Workstation.belongsTo(Inventory, { foreignKey: 'inventory_id' });
+
+Inventory.hasMany(CCTV, { foreignKey: 'inventory_id' });
+CCTV.belongsTo(Inventory, { foreignKey: 'inventory_id' });
+
+Workstation.hasMany(JobHostSummary, { foreignKey: 'workstation_id' });
+JobHostSummary.belongsTo(Workstation, { foreignKey: 'workstation_id' });
+
+CCTV.hasMany(JobHostSummary, { foreignKey: 'cctv_id' });
+JobHostSummary.belongsTo(CCTV, { foreignKey: 'cctv_id' });
+
+Job.hasMany(JobHostSummary, { foreignKey: 'job_id' });
+JobHostSummary.belongsTo(Job, { foreignKey: 'job_id' });
+
+
+export { sequelize, Filial, Inventory, Workstation, CCTV, Job, JobHostSummary };
