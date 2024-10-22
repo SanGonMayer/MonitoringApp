bdd8c5f451237208aa08333df756fae42a6a63c1
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-10-22 11:04:36 -0300

    default routes

--- awx-monitoring-backend/routes/awxRoutes.js
+++ awx-monitoring-backend/routes/awxRoutes.js
@@ -6,3 +6,5 @@ export const awxRoutes = Router();
 awxRoutes.get('/api/awx/inventories/:inventoryId/groups', GroupHostController.getGroups);
 awxRoutes.get('/api/awx/inventories/:inventoryId/groups/:groupId/hosts', GroupHostController.getHosts);
 
+export default awxRoutes;
+
