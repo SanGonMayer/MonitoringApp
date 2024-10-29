bd102c31ddd4ba0c866fb7bd705bd4350cb3d4f8
TommyChinchay <tchinchay@frba.utn.edu.ar>
2024-10-29 16:37:53 -0300

    actualizando busqueda

--- front_wst_control/scripts/appNEW.js
+++ front_wst_control/scripts/appNEW.js
@@ -1,8 +1,18 @@
+/* function buscar(tipoTerminal) {
+    if (tipoTerminal === 'cctv.html') {
+        fetchFilialesFromDB('cctv.html');
+    } else if (tipoTerminal === 'wst.html') {
+        fetchFilialesFromDB('wst.html');
+    } else {
+        console.log('Página no reconocida.');
+    }
+}
+ */
 function buscar(tipoTerminal) {
     if (tipoTerminal === 'cctv.html') {
-        fetchFiliales(347);
+        fetchFilialesFromDB('cctv.html');
     } else if (tipoTerminal === 'wst.html') {
-        fetchFiliales(22);
+        fetchFilialesFromDB('wst.html');
     } else if (tipoTerminal === ''){
         crearGraficoCircular('#circularCctv','cctv.html');
         crearGraficoCircular('#circularWst','wst.html');
