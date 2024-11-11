261ad63083676c1ba0c6f820f4945f4d2c06e019
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-11-11 12:19:51 -0300

    ruta, como siempre

--- awx-monitoring-backend/routes/awxRoutes.js
+++ awx-monitoring-backend/routes/awxRoutes.js
@@ -6,6 +6,7 @@ import { getFilialesFromDB } from '../services/dbService.js';
 import { getHostsByFilial } from '../controllers/hostsController.js';
 import { getFilialesConHosts } from '../services/dbService.js';
 import { updateSingleFilial } from '../controllers/syncController.js';
+import { launchJob } from '../controllers/awxController.js';
 
 export const awxRoutes = Router();
 
@@ -50,6 +51,7 @@ awxRoutes.get('/api/db/filiales/:tipoTerminal', async (req, res) => {
 })
 
 awxRoutes.post('/api/sync/filial/:filialId', updateSingleFilial);
+awxRoutes.post('/api/awx/launch-job', launchJob);
 
 export default awxRoutes;
 
