cdd308858ecb5add6e1eac4fa6dadcec1cf7ebdf
TommyChinchay <tchinchay@frba.utn.edu.ar>
2024-11-13 11:19:38 -0300

    Corrigiendo para que evalue wst ya que snro es de wst solo que esta separado para otro platilla

--- front_wst_control/scripts/appNEW.js
+++ front_wst_control/scripts/appNEW.js
@@ -6,7 +6,7 @@ async function buscar(tipoTerminal) {
     } else if (tipoTerminal === 'wst.html') {
         await fetchFilialesConHostsFromDB('wst.html');
     } else if(tipoTerminal === 'srno.html'){
-        await fetchFilialesConHostsFromDB('srno.html');
+        await fetchFilialesConHostsFromDB('wst.html');
     } else if (tipoTerminal === ''){
         await crearGraficoCircular('#circularCctv','cctv.html');
         await crearGraficoCircular('#circularWst','wst.html');
