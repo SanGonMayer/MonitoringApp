2281f8b67b8c6d364ec404e5f44492816296bd33
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-12-30 16:09:54 -0300

    agrego snapshot a ruta de sync para testing

--- awx-monitoring-backend/routes/awxRoutes.js
+++ awx-monitoring-backend/routes/awxRoutes.js
@@ -19,6 +19,7 @@ awxRoutes.post('/api/sync', async (req, res) => {
     try {
         await startDataSync();
         res.status(200).json({ message: 'Sincronización de datos completada.' });
+        await takeDailySnapshot();
     } catch (error) {
         console.error('Error en la sincronización manual:', error.message);
         res.status(500).json({ error: 'Error en la sincronización manual: ' + error.message });
