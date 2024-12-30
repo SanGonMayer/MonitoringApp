f0a1e6020fd1e3f981eb9bdad493cff715c65461
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-12-29 22:17:00 -0300

    el import

--- awx-monitoring-backend/models/index.js
+++ awx-monitoring-backend/models/index.js
@@ -7,7 +7,8 @@ import Workstation from './workstations.js';
 import CCTV from './cctv.js';
 //import Job from './jobs.js';
 import JobHostSummary from './jobHostSummary.js';
-//import HostStatusHistory from './hostStatusHistory.js';
+import HostSnapshot from './hostsSnapshot.js';
+
 
 Filial.hasMany(Workstation, { foreignKey: 'filial_id' });
 Workstation.belongsTo(Filial, { foreignKey: 'filial_id' });
