b0cf666219e2969c280bc6222cbebaa246b473c9
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2025-01-07 19:14:07 -0300

    seteo bien la ruta para generar bien el snapshot

--- awx-monitoring-backend/routes/awxRoutes.js
+++ awx-monitoring-backend/routes/awxRoutes.js
@@ -9,6 +9,7 @@ import { updateSingleFilial } from '../controllers/syncController.js';
 import { launchJob } from '../controllers/awxController.js';
 import { sendTestTelegramMessage } from '../controllers/notifierController.js';
 import { getLatestCSV } from '../services/notificadorService.js';
+import { takeDailySnapshot } from '../services/snapshotService.js';
 
 export const awxRoutes = Router();
 
@@ -18,8 +19,8 @@ awxRoutes.get('/api/awx/inventories/:inventoryId/groups/:groupId/hosts', GroupHo
 awxRoutes.post('/api/sync', async (req, res) => {
     try {
         await startDataSync();
-        res.status(200).json({ message: 'Sincronización de datos completada.' });
         await takeDailySnapshot();
+        res.status(200).json({ message: 'Sincronización de datos completada.' });
     } catch (error) {
         console.error('Error en la sincronización manual:', error.message);
         res.status(500).json({ error: 'Error en la sincronización manual: ' + error.message });
