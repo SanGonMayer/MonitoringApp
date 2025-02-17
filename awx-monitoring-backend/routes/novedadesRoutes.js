5bd6ca173438c0c2ec3dc205636a43b30d56197b
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2025-02-17 14:16:34 -0300

    agrego tambien los pasos de deshab a hab

--- awx-monitoring-backend/routes/novedadesRoutes.js
+++ awx-monitoring-backend/routes/novedadesRoutes.js
@@ -20,7 +20,9 @@ router.get('/agregados', async (req, res) => {
 
     const agregados = await HostSnapshot.findAll({
       where: {
-        motivo: 'Host agregado',
+        motivo: {
+          [Op.in]: ['Host agregado', 'Modificacion de desactualizado a actualizado']
+        },
         snapshot_date: {
           [Op.gte]: startOfToday,
           [Op.lte]: endOfToday,
